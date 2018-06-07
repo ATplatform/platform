4749b45ef2df0ed75ccf488ae471be20bd2c7811
ZhaiJunhao <499158415@qq.com>
2018-06-07 18:11:21 +0800

    xx

---
+++ platform8/application/views/app/menus.php
@@ -0,0 +1,266 @@
+<div class="leftNav col-sm-12 pl0 pr0 ml0 mr0">
+	<ul id="main-nav" class="nav nav-tabs nav-stacked" style="">
+
+	<li>
+		<a href="#BuildingSetting" class="nav-header " data-toggle="collapse"
+			<?php if($nav=="buildingtree"||$nav=="buildinglist") {echo 'aria-expanded="true"';} ?> 
+		>
+		<i class="icon fa fa-building"></i>
+			楼宇管理
+			<span class="pull-right fa fa-chevron-right"></span> 
+		</a>
+		<ul id="BuildingSetting" 
+			<?php 
+				if($nav=="buildingtree"||$nav=="buildinglist"){
+					echo 'class="collapse secondmenu in"';
+				} 
+				else{
+					echo 'class="collapse secondmenu"';
+				} 
+			?>   
+		aria-expanded="true" >
+			<li><a href="<?=base_url().'index.php/Building/notfond'?>"><i class="glyphicon"></i>社区信息管理</a></li>
+			<li <?php if($nav=="buildingtree"||$nav=="buildinglist") {echo 'class="active"';} ?> ><a href="<?=base_url().'index.php/Building/buildingtree'?>"><i class="glyphicon"></i>楼宇关系管理</a></li>
+		</ul>
+	</li>
+
+	<li>
+		<a href="#systemSetting" class="nav-header " data-toggle="collapse"
+			<?php if($nav=="residentlist") {echo 'aria-expanded="true"';} ?> 
+		>
+		<i class="icon fa fa-user-circle"></i>
+			人员管理
+			<span class="pull-right fa fa-chevron-right"></span> 
+		</a>
+		<ul id="systemSetting" 
+			<?php 
+				if($nav=="residentlist"||$nav=="managementlist"||$nav=="businesslist"){
+					echo 'class="collapse secondmenu in"';
+				} 
+				else{
+					echo 'class="collapse secondmenu"';
+				} 
+			?>   
+		aria-expanded="true" >
+			<li <?php if($nav=="residentlist") {echo 'class="active"';} ?> ><a href="<?=base_url().'index.php/People/residentlist'?>"><i class="glyphicon"></i>住户信息管理</a></li>
+			<li <?php if($nav=="managementlist") {echo 'class="active"';} ?> ><a href="<?=base_url().'index.php/People/managementlist'?>"><i class="glyphicon"></i>物业人员管理</a></li>
+			<li <?php if($nav=="businesslist") {echo 'class="active"';} ?> ><a href="<?=base_url().'index.php/People/businesslist'?>"><i class="glyphicon"></i>商户人员管理</a></li>
+		</ul>
+	</li>
+
+	<li>
+		<a href="#EquipmentSetting" class="nav-header " data-toggle="collapse"
+			<?php if($nav=="equipmentlist"||$nav=="personequipmentlist") {echo 'aria-expanded="true"';} ?> 
+		>
+		<i class="icon fa fa-archive"></i>
+			设备管理
+			<span class="pull-right fa fa-chevron-right"></span> 
+		</a>
+		<ul id="EquipmentSetting" 
+			<?php 
+				if($nav=="equipmentlist"||$nav=="personequipmentlist"){
+					echo 'class="collapse secondmenu in"';
+				} 
+				else{
+					echo 'class="collapse secondmenu"';
+				} 
+			?>   
+		aria-expanded="true" >
+			<li><a href="<?=base_url().'index.php/Building/notfond'?>"><i class="glyphicon"></i>设备运营分析</a></li>
+			<li <?php if($nav=="equipmentlist") {echo 'class="active"';} ?> ><a href="<?=base_url().'index.php/Equipment/equipmentlist'?>"><i class="glyphicon"></i>设备管理列表</a></li>
+			<li <?php if($nav=="personequipmentlist") {echo 'class="active"';} ?>><a href="<?=base_url().'index.php/Equipment/personequipmentlist'?>"><i class="glyphicon"></i>设备授权管理</a></li>
+			<li><a href="<?=base_url().'index.php/Building/notfond'?>"><i class="glyphicon"></i>设备状态管理</a></li>
+			<li><a href="<?=base_url().'index.php/Building/notfond'?>"><i class="glyphicon"></i>设备维保管理</a></li>
+		</ul>
+	</li>
+
+	<li>
+	    <a href="#Workorder" class="nav-header " data-toggle="collapse"
+	        <?php if($nav=="workorderList") {echo 'aria-expanded="true"';} ?>
+	    >
+
+	        <i class="icon fa fa-file"></i>
+	        工单管理
+	        <span class="pull-right fa fa-chevron-right"></span>
+	    </a>
+	    <ul id="Workorder"
+	        <?php
+	        if($nav=="workorderList"){
+	            echo 'class="collapse secondmenu in"';
+	        }
+	        else{
+	            echo 'class="collapse secondmenu"';
+	        }
+	        ?>
+	        aria-expanded="true" >
+	        <li <?php if($nav=="workorderList") {echo 'class="active"';} ?> ><a href="<?=base_url().'index.php/Workorder/workorderList'?>"><i class="glyphicon"></i>工单记录</a></li>
+
+	    </ul>
+	</li>
+
+	<li>
+		<a href="#MessageSetting" class="nav-header " data-toggle="collapse"
+			<?php if($nav=="messagelist") {echo 'aria-expanded="true"';} ?> 
+		>
+		<i class="icon fa fa-envelope"></i>
+			信息管理
+			<span class="pull-right fa fa-chevron-right"></span> 
+		</a>
+		<ul id="MessageSetting" 
+			<?php 
+				if($nav=="messagelist"||$nav=="messagerecordlist"){
+					echo 'class="collapse secondmenu in"';
+				} 
+				else{
+					echo 'class="collapse secondmenu"';
+				} 
+			?>   
+		aria-expanded="true" >
+			<li <?php if($nav=="messagelist") {echo 'class="active"';} ?> ><a href="<?=base_url().'index.php/Message/messagelist'?>"><i class="glyphicon"></i>信息条目管理</a></li>
+			<li <?php if($nav=="messagerecordlist") {echo 'class="active"';} ?> ><a href="<?=base_url().'index.php/Message/messagerecordlist'?>"><i class="glyphicon"></i>信息记录管理</a></li>
+		</ul>
+	</li>
+
+	<li>
+		<a href="#VehicleSetting" class="nav-header " data-toggle="collapse"
+		>
+		<i class="icon fa fa-car"></i>
+			停车管理
+			<span class="pull-right fa fa-chevron-right"></span> 
+		</a>
+		<ul id="VehicleSetting" 
+			<?php 
+				if($nav=="vehiclelist"){
+					echo 'class="collapse secondmenu in"';
+				} 
+				else{
+					echo 'class="collapse secondmenu"';
+				} 
+			?>   
+		aria-expanded="true" >
+			<li <?php if($nav=="vehiclelist") {echo 'class="active"';} ?> ><a href="<?=base_url().'index.php/Building/notfond'?>"><i class="glyphicon"></i>车辆信息管理</a></li>
+			<li><a href="<?=base_url().'index.php/Building/notfond'?>"><i class="glyphicon"></i>车辆进出记录</a></li>
+			<li><a href="<?=base_url().'index.php/Building/notfond'?>"><i class="glyphicon"></i>车辆缴费记录</a></li>
+		</ul>
+	</li>
+
+	<li>
+		<a href="#FinanceSetting" class="nav-header " data-toggle="collapse"
+		>
+		<i class="icon fa fa-money"></i>
+			财务管理
+			<span class="pull-right fa fa-chevron-right"></span> 
+		</a>
+		<ul id="FinanceSetting" 
+			<?php 
+				if($nav=="vehiclelist"){
+					echo 'class="collapse secondmenu in"';
+				} 
+				else{
+					echo 'class="collapse secondmenu"';
+				} 
+			?>   
+		aria-expanded="true" >
+			<li><a href="<?=base_url().'index.php/Building/notfond'?>"><i class="glyphicon"></i>社区经营分析</a></li>
+			<li><a href="<?=base_url().'index.php/Building/notfond'?>"><i class="glyphicon"></i>车辆缴费管理</a></li>
+			<li><a href="<?=base_url().'index.php/Building/notfond'?>"><i class="glyphicon"></i>物业费缴费管理</a></li>
+			<li><a href="<?=base_url().'index.php/Building/notfond'?>"><i class="glyphicon"></i>其他费用缴费管理</a></li>
+		</ul>
+	</li>
+
+	<li>
+	    <a href="#material" class="nav-header " data-toggle="collapse"
+	        <?php if($nav=="materialList") {echo 'aria-expanded="true"';} ?>
+	    >
+
+	        <i class="icon fa fa-gift"></i>
+	        物资管理
+	        <span class="pull-right fa fa-chevron-right"></span>
+	    </a>
+	    <ul id="material"
+	        <?php
+	        if($nav=="materialList"||$nav=="materialUsage"){
+	            echo 'class="collapse secondmenu in"';
+	        }
+	        else{
+	            echo 'class="collapse secondmenu"';
+	        }
+	        ?>
+	        aria-expanded="true" >
+	        <li <?php if($nav=="materialList") {echo 'class="active"';} ?> ><a href="<?=base_url().'index.php/Material/materialList'?>"><i class="glyphicon"></i>物资列表</a></li>
+	        <li <?php if($nav=="materialUsage") {echo 'class="active"';} ?> ><a href="<?=base_url().'index.php/Material/materialUsage'?>"><i class="glyphicon"></i>物资使用管理</a></li>
+	    </ul>
+	</li>
+
+	<li>
+	    <a href="#ContractSetting" class="nav-header " data-toggle="collapse"
+	        <?php if($nav=="contractList") {echo 'aria-expanded="true"';} ?>
+	    >
+
+	        <i class="icon fa fa-reorder"></i>
+	        合同管理
+	        <span class="pull-right fa fa-chevron-right"></span>
+	    </a>
+	    <ul id="ContractSetting"
+	        <?php
+	        if($nav=="contractList"){
+	            echo 'class="collapse secondmenu in"';
+	        }
+	        else{
+	            echo 'class="collapse secondmenu"';
+	        }
+	        ?>
+	        aria-expanded="true" >
+	        <li <?php if($nav=="contractList") {echo 'class="active"';} ?> ><a href="<?=base_url().'index.php/Contract/contractList'?>"><i class="glyphicon"></i>合同管理列表</a></li>
+	    </ul>
+	</li>
+
+	<li>
+	    <a href="#ActivitySetting" class="nav-header " data-toggle="collapse"
+	        <?php if($nav=="activityList") {echo 'aria-expanded="true"';} ?>
+	    >
+
+	        <i class="icon fa fa-font-awesome"></i>
+	        住户活动管理
+	        <span class="pull-right fa fa-chevron-right"></span>
+	    </a>
+	    <ul id="ActivitySetting"
+	        <?php
+	        if($nav=="activityList"){
+	            echo 'class="collapse secondmenu in"';
+	        }
+	        else{
+	            echo 'class="collapse secondmenu"';
+	        }
+	        ?>
+	        aria-expanded="true" >
+	        <li <?php if($nav=="activityList") {echo 'class="active"';} ?> ><a href="<?=base_url().'index.php/Building/notfond'?>"><i class="glyphicon"></i>住户活动圈</a></li>
+	        <li><a href="<?=base_url().'index.php/Building/notfond'?>"><i class="glyphicon"></i>住户活动管理</a></li>
+	    </ul>
+	</li>
+
+	<li>
+	    <a href="#FunctionSetting" class="nav-header " data-toggle="collapse"
+	        <?php if($nav=="activityList") {echo 'aria-expanded="true"';} ?>
+	    >
+
+	        <i class="icon fa fa-shopping-cart"></i>
+	        应用管理
+	        <span class="pull-right fa fa-chevron-right"></span>
+	    </a>
+	    <ul id="FunctionSetting"
+	        <?php
+	        if($nav=="functionList"){
+	            echo 'class="collapse secondmenu in"';
+	        }
+	        else{
+	            echo 'class="collapse secondmenu"';
+	        }
+	        ?>
+	        aria-expanded="true" >
+	        <li><a href="<?=base_url().'index.php/Building/notfond'?>"><i class="glyphicon"></i>二维码打印</a></li>
+	    </ul>
+	</li>
+
+</ul>
+</div>
---
+++ platform8/application/views/plugin/app/js/messagerecord_list.js
@@ -0,0 +1,96 @@
+//日期控件初始化
+var now = new Date();
+now = formatDate(now);
+var table = $('#table');
+//带search的参数用于异步刷新页面时做分页和查询总条数
+var search_keyword = getUrlParam('keyword');
+var search_start_date = getUrlParam('push_start_date');
+var search_end_date = getUrlParam('push_end_date');
+var search_msg_type = getUrlParam('msg_type');
+var search_cycle_type = getUrlParam('cycle_type');
+var search_push_state = getUrlParam('push_state');
+
+//推送时间精确到分
+$('.push_start_date,.push_end_date,.end_date').datetimepicker({
+    language:  'zh-CN',
+    format: 'yyyy-mm-dd hh:ii',
+    weekStart: 1,
+    todayBtn:  1,
+	autoclose: 1,
+	todayHighlight: 1,
+	startView: 2,
+	forceParse: 1,
+	minView:0,
+    showMeridian: 1
+});
+
+//信息管理操作
+function operateFormatter(value,row,index){
+	return [
+	    '<a class="detail" href="javascript:void(0)" style="margin-left: 10px;" title="详情">',
+	    '<i class=" fa fa-trash-o fa-lg fa-file-text-o"></i>',
+	    '</a>'
+	].join('');
+}
+window.operateEvents = {
+	//点击详情时,弹出信息详情框
+	'click .detail':function(e,value,row,index){
+		$('#message_detail').modal('show');
+		console.log(row);
+		var code = row.code;
+		var cycle_type_name = row.cycle_type_name;
+		var if_bill_name = row.if_bill_name;
+		var bill_amount = row.bill_amount;
+		var if_receipt_name = row.if_receipt_name;
+		var msg_type_name = row.msg_type_name;
+		var link = row.link;
+		var msg_title = row.msg_title;
+		var push_start_date = row.push_time;
+		var push_end_date = row.push_end_date;
+		var target = row.target;
+		var target_arr = [];
+		//传给后端数组,方便查找
+		target = target.substr(1);
+		target = target.substr(0,target.length-1);
+		target_arr = target.split(',');
+
+		$('#message_detail').find('.code').html(code);
+		$('#message_detail').find('.cycle_type_name').html(cycle_type_name);
+		$('#message_detail').find('.if_bill_name').html(if_bill_name);
+		$('#message_detail').find('.bill_amount em').html(bill_amount);
+		$('#message_detail').find('.if_receipt_name').html(if_receipt_name);
+		$('#message_detail').find('.msg_type_name').html(msg_type_name);
+		$('#message_detail').find('.msg_title').html(msg_title);
+		$('#message_detail').find('.push_start_date').html(push_start_date);
+		$('#message_detail').find('.push_end_date').html(push_end_date);
+		$('#message_detail').find('.msg_html').attr('src',link);
+		//得到所有的推送对象
+		$.ajax({
+			url:getRootPath()+'/index.php/Message/getMessageTarget',
+			method:'post',
+			dataType:'json',
+			data:{
+				target:target_arr
+			},
+			success:function(data){
+				// console.log(data);
+				// var data = JSON.parse(data);
+				if(data){
+					var html_tmp = "";
+					console.log(data);
+					for(var i=0;i<data.length;i++){
+						html_tmp += "<em data-room_code='"+data[i]['buildings']['room_code']+"'>"+data[i]['household']+"</em>";
+					}	
+					$('#message_detail .target_wrap').empty();			
+					$('#message_detail .target_wrap').append(html_tmp);	
+				}
+				else{
+					$('#message_detail .target_wrap').empty();
+				}
+			},
+			error:function(){
+				console.log('数据出错');
+			}
+		})	
+	}
+}
